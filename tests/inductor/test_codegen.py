78d7a06d40ba0fe19544c48b2ad8c8f3b7e12c58
Justin Turney <justin.turney@gmail.com>
2026-04-03 15:32:23 +0000

    sencore test
    Signed-off-by: Justin Turney <justin.turney@gmail.com>

--- tests/inductor/test_codegen.py
+++ tests/inductor/test_codegen.py
@@ -14,7 +14,6 @@
 
 import torch
 from torch_spyre._inductor import config
-from utils_inductor import cached_randn
 from torch.testing import FileCheck
 from torch._inductor.exc import InductorError
 from torch._inductor.test_case import TestCase as InductorTestCase
@@ -26,16 +25,20 @@ from torch._inductor.utils import (
 class TestSpyreConfig(InductorTestCase):
     def test_config_default(self):
         fn = torch.abs
-        x = cached_randn((256,)).to("spyre")
+        x = torch.randn((256, 128, 512)).to("spyre")
 
         comp_fn = torch.compile(fn)
         out, source_codes = run_and_get_code(comp_fn, x)
-        FileCheck().check("sdsc_fused_abs_0").run(source_codes[0])
+        # print("test_config_default")
+        # print(source_codes[0])
+        FileCheck().check("sdsc_fused_abs").check(
+            f"sympify('c0'): (sympify('256'), {config.sencores})"
+        ).run(source_codes[0])
 
     @config.patch({"sencores": 64})
     def test_config_too_many_sencores(self):
         fn = torch.abs
-        x = cached_randn((256,)).to("spyre")
+        x = torch.randn((256, 128, 512)).to("spyre")
 
         with self.assertRaisesRegex(
             InductorError,
@@ -44,13 +47,36 @@ class TestSpyreConfig(InductorTestCase):
             comp_fn = torch.compile(fn)
             comp_fn(x)
 
-    # Need a test where getting lx_planning to True generates a different kernel
-    # @config.patch({'lx_planning': True})
-    # def test_config_lx_planning(self):
+    @config.patch({"sencores": 16})
+    def test_sencores_16(self):
+        fn = torch.abs
+        x = torch.randn((256, 128, 512)).to("spyre")
+        cfn = torch.compile(fn, dynamic=False)
+        out, source_codes = run_and_get_code(cfn, x)
+        # print("test_sencores 16")
+        # print(source_codes[0])
+        FileCheck().check("sdsc_fused_abs").check(
+            f"sympify('c0'): (sympify('256'), {config.sencores})"
+        ).run(source_codes[0])
+
+    # Need a test where changing dxp_lx_frac_avail changes the generated OpSpec
+    # @config.patch({"dxp_lx_frac_avail": 0.01, "lx_planning": True})
+    # def test_config_dxp_lx_frac_avail(self):
     #    fn = torch.abs
-    #    x = cached_randn((256,)).to("spyre")
+    #    x = torch.randn((256, 128, 512)).to("spyre")
     #
     #    comp_fn = torch.compile(fn)
     #    out, source_codes = run_and_get_code(comp_fn, x)
-    #    print(f"lx_planning {config.lx_planning}")
-    #    print(source_codes[0])
+    #    #print("test_conf_dxp_lx_frac_avail")
+    #    #print(source_codes[0])
+
+    # Need a test where setting lx_planning to True generates a different OpSpec
+    # @config.patch({'lx_planning': True})
+    # def test_config_lx_planning(self):
+    #    fn = torch.abs
+    #    x = torch.randn((256, 128, 512)).to("spyre")
+    #
+    #    comp_fn = torch.compile(fn)
+    #    out, source_codes = run_and_get_code(comp_fn, x)
+    #    #print(f"lx_planning {config.lx_planning}")
+    #    #print(source_codes[0])
